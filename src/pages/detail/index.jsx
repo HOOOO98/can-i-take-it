5c0274860781ca32db651890ddb1816a3dcf8280
HOOOO98 <loading2us@gmail.com>
2024-01-30 23:02:21 +0900

    Conf: initial settings

---
+++ src/pages/detail/index.jsx
@@ -0,0 +1,14 @@
+import Head from "next/head";
+
+function Detail() {
+  return (
+    <>
+      <Head>
+        <title>CallVeda | Detail</title>
+      </Head>
+      <div>Detail</div>
+    </>
+  );
+}
+
+export default Detail;
---
+++ src/pages/index.jsx
@@ -0,0 +1,15 @@
+import Head from "next/head";
+import React from "react";
+
+function Home() {
+  return (
+    <>
+      <Head>
+        <title>CallVeda | Home</title>
+      </Head>
+      <div>Home</div>
+    </>
+  );
+}
+
+export default Home;
