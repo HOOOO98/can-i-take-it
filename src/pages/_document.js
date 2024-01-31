6c7ea7b41bdf2da0001053daaca6caf7bfb81680
HOOOO98 <loading2us@gmail.com>
2024-01-31 16:15:17 +0900

    Conf: new initial settings

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,6 +1,6 @@
 import { Html, Head, Main, NextScript } from "next/document";
 
-function Document() {
+export default function Document() {
   return (
     <Html lang="en">
       <Head />
@@ -11,5 +11,3 @@ function Document() {
     </Html>
   );
 }
-
-export default Document;
