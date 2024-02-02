86fb48c017ed880f815abcb36ce3e7b78b511fa9
HOOOO98 <loading2us@gmail.com>
2024-02-02 15:45:10 +0900

    Feat: add sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,11 @@
 const sitemapConfig = {
   siteUrl: "https://www.canitakeit.vercel.app",
   generateRobotsTxt: true,
+  extraPaths: async () => {
+    const numbers = ["123456789", "987654321"];
+    const paths = numbers.map((number) => `/details/${number}`);
+    return paths;
+  },
 };
 
 module.exports = sitemapConfig;
